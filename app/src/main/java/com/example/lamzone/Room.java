ea918897f14d8941e0618d1ab716e4ff8bb14507
Glenn-Richard <bglennrichard.pro@yahoo.com>
2021-04-10 13:25:57 +0200

    [Room] creation of class

---
+++ app/src/main/java/com/example/lamzone/Room.java
@@ -0,0 +1,32 @@
+package com.example.lamzone;
+
+public class Room {
+
+    //VARIABLES
+
+    private int id;
+    private String name;
+
+    //CONSTRUCTOR
+    public Room(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //GETTERS AND SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
