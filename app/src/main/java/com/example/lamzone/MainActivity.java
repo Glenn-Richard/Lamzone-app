ed4bf46946fcaa302c41ae8a566784b53b87b5e3
Glenn-Richard <bglennrichard.pro@yahoo.com>
2021-04-10 12:44:29 +0200

    [MeetingAdapter] create class adapter

--- app/src/main/java/com/example/lamzone/MainActivity.java
+++ app/src/main/java/com/example/lamzone/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lamzone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.recyclerview_list);
+
+        final RecyclerView rv = findViewById(R.id.list_item_rv);
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new MeetingAdapter());  ,
     }
 }
---
+++ app/src/main/java/com/example/lamzone/MeetingAdapter.java
@@ -0,0 +1,7 @@
+package com.example.lamzone;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.ViewHolder> {
+
+}
