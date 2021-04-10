d617dbeea5c6cd611fb3950d9a5aadac0ad37188
Glenn-Richard <bglennrichard.pro@yahoo.com>
2021-04-10 13:22:24 +0200

    [Meeting] creation of class

--- app/src/main/java/com/example/lamzone/ListActivity.java
+++ app/src/main/java/com/example/lamzone/ListActivity.java
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/lamzone/Meeting.java
@@ -0,0 +1,55 @@
+package com.example.lamzone;
+
+import java.util.List;
+
+public class Meeting {
+
+    //VARIABLES
+    private  int id;
+    private long timestamp;
+    private String subject;
+    private List<String> emails;
+
+
+    //CONSTRUCTOR
+    public Meeting(int id, long timestamp, String subject, List<String> emails) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.subject = subject;
+        this.emails = emails;
+    }
+
+    //GETTER AND SETTER
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
+}
+
